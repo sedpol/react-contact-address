eca7268e8233af9b7649e32fdb74f5e9cbd8b284
sedpol <sedpol@gmail.com>
2018-09-23 22:33:07 +0100

    add key on service response

--- src/Services/AddressServices.js
+++ src/Services/AddressServices.js
@@ -4,7 +4,15 @@ export class AddressService {
 
     getAllAddresses() {
         return axios.get('https://jsonplaceholder.typicode.com/users')
-                .then(result => result.data);
+            .then(result => {
+                const response = [];
+                let i = 0;
+                result.data.forEach(element => {
+                    const el = {...element, key: element.id}
+                    response[i++] = (el);
+                });
+                return response;
+            });
     }
 }
 
